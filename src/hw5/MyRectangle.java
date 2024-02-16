03f38af7cb2b95f9732064e161ada2c3d6c76206
peihui <pride97350@gmail.com>
2024-02-16 22:43:12 +0800

    Revise annotation

--- src/hw5/MyRectangle.java
+++ src/hw5/MyRectangle.java
@@ -4,20 +4,36 @@ public class MyRectangle {
     private double width;
     private double depth;
 
+    /**
+     * 將收到的引數指定給width屬性
+     *
+     * @param width 長方形寬
+     */
     public void setWidth(double width) {
         this.width = width;
     }
 
+    /**
+     * 將收到的引數指定給depth屬性
+     *
+     * @param depth 長方形高
+     */
     public void setDepth(double depth) {
         this.depth = depth;
     }
 
+    /**
+     * 計算該長方形的面積
+     *
+     * @return 面積
+     */
     public double getArea() {
         return width * depth;
     }
 
     public MyRectangle() {}
 
+    // 傳入的兩個引數會指定給對應的屬性
     public MyRectangle(double width, double depth) {
         setWidth(width);
         setDepth(depth);
--- src/hw6/Calculator.java
+++ src/hw6/Calculator.java
@@ -2,6 +2,14 @@ package hw6;
 
 public class Calculator {
 
+    /**
+     * 計算x的y次方
+     *
+     * @param x 底數
+     * @param y 指數
+     * @return 運算結果
+     * @throws CalException 自定義例外訊息
+     */
     public static int powerXY(int x, int y) throws CalException {
         if (x == 0 && y == 0) {
             throw new CalException("0的0次方沒有意義!");
